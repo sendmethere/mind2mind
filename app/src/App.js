bab25d832f8a87b0c6aab5869aa72d28ed63a768
sendmethere <sendmethere@naver.com>
2022-08-22 14:21:48 +0900

    tailwind css added

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
     </div>
   );
 }
