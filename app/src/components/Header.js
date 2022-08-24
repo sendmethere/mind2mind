bac1e5d21c749f2a6f2447bec139ae328250d351
taaaaes <sendmethere@naver.com>
2022-08-24 18:23:30 +0900

    deploy complete

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -4,8 +4,8 @@ export default function Navbar() {
         <div className='mx-auto'>
                 <div className="flex justify-between items-center border-b-2 border-gray-100 
                                 py-6 md:justify-center md:space-x-10">
-                        <a href="/maker/" className="text-base font-medium text-gray-500 hover:text-sky-600">만들기</a>
                         <a href="https://solar-bone-db8.notion.site/73d91fc4460648ad9ac97257c27c980e" className="text-base font-medium text-gray-500 hover:text-sky-600">도움말</a>
+                        <a href="https://www.notion.so/4ea76e7d1d2742bcb579a61e64126297" className="text-base font-medium text-gray-500 hover:text-sky-600">만든 이</a>
                 </div>
         </div>
    </div>
