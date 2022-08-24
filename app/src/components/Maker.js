eba1db2562bc327b29876d35b69abc60cff4e096
taaaaes <sendmethere@naver.com>
2022-08-24 18:15:26 +0900

    deploy 5

--- app/src/components/Maker.js
+++ app/src/components/Maker.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
+
+import Footer from './Footer';
+import Navbar from './Header';
+
 import Choice from './Choice';
 import Icon from './Icon';
 import IconList from './IconList';
 import logo from '../logo.svg';
+
 
 function Maker(props) {
 
@@ -140,62 +145,78 @@ function Maker(props) {
 
 
     return (
-        <div className='md:flex justify-center' >
-            <div className="grow">
-                <div className="flex justify-center text-center">
-                    <img className="w-44 m-auto" src={logo}></img>
-                </div>
-                
-                <div className={`grow ${game.started ? "hidden" : ""}`}>
-                    {suggestions.map((choice, index)=>{
-                        return(
-                            <Choice clickEvent={pickAnswer} key={index} no={index+1} answer={choice} text={numberEmoji[index] + choice} isChosen={ (index+1 == answer.no) }></Choice>
-                        )
-                    })}
-                    <Choice clickEvent={shuffle} text="🎲 다른 문제 보기" ></Choice>
-                </div>
 
-                
-                <div className={`${game.started ? "" : "hidden"}`}>
-                    <div className='p-6 flex justify-center'>
-                        <input className="rounded border p-5 text-xl" type="text" onChange={handleChange} />
-                    </div>
-                    <div className={`${(game.status === "WRONG") ? "" : "hidden"} m-auto rounded p-2 m-2 bg-red-100 text-red-400 w-5/6`}>오답입니다!</div>
-                    <div className={`${(game.status === "CORRECT") ? "" : "hidden"} m-auto rounded p-2 m-2 bg-green-100 text-green-600 w-5/6`}>정답입니다!</div>
-                    <div className='flex justify-center'>
-                        <div className={`rounded border border-gray-300 font-bold px-6 py-4 m-2 hover:cursor-pointer hover:brightness-105`} onClick={intialize}>돌아가기</div>
-                        <div className={`rounded bg-sky-300 font-bold px-6 py-4 m-2 hover:cursor-pointer hover:brightness-105`} onClick={checkAnswer}>확인</div>
-                    </div>
-                </div>
+        <div className="md:w-screen md:h-screen"
+        style={{"background": "#f4f5f6"}} >
+            <Navbar/>
+            <div className='flex justify-center py-8'>
+                <div className='rounded bg-white px-4 py-4'
+                     style={{width:"840px"}}>
 
-
-
-            </div>
-            <div style={{width: "480px"}} >
-                <div className="grid grid-rows-4 gap-3">
-                    {icons.map((row, index) => {
-                        return (
-                                <div key={index} style={{background: colors[index]}} className="rounded p-2 grid grid-cols-5 gap-3">
-                                    <div className="flex items-center"><div className="text-center grow text-2xl text-bold text-white">{index+1}</div></div>
-
-                                    {row.map((icon, index_i) => {
-                                        return (
-                                            <Icon key={index*4+index_i} row={index} no={index_i} icon={icons[index][index_i]["icon"]} desc={icons[index][index_i]["desc"]} onOpenModal={onOpenModal}/>
-                                        )
-                                    })}
+                    <div className='md:flex justify-center' >
+                                <div className="grow">
+                                    <div className="flex justify-center text-center">
+                                        <img className="w-44 m-auto" src={logo}></img>
+                                    </div>
                                     
+                                    <div className={`grow ${game.started ? "hidden" : ""}`}>
+                                        {suggestions.map((choice, index)=>{
+                                            return(
+                                                <Choice clickEvent={pickAnswer} key={index} no={index+1} answer={choice} text={numberEmoji[index] + choice} isChosen={ (index+1 == answer.no) }></Choice>
+                                            )
+                                        })}
+                                        <Choice clickEvent={shuffle} text="🎲 다른 문제 보기" ></Choice>
+                                    </div>
+
+                                    
+                                    <div className={`${game.started ? "" : "hidden"}`}>
+                                        <div className='p-6 flex justify-center'>
+                                            <input className="rounded border p-5 text-xl" type="text" onChange={handleChange} />
+                                        </div>
+                                        <div className={`${(game.status === "WRONG") ? "" : "hidden"} m-auto rounded p-2 m-2 bg-red-100 text-red-400 w-5/6`}>오답입니다!</div>
+                                        <div className={`${(game.status === "CORRECT") ? "" : "hidden"} m-auto rounded p-2 m-2 bg-green-100 text-green-600 w-5/6`}>정답입니다!</div>
+                                        <div className='flex justify-center'>
+                                            <div className={`rounded border border-gray-300 font-bold px-6 py-4 m-2 hover:cursor-pointer hover:brightness-105`} onClick={intialize}>돌아가기</div>
+                                            <div className={`rounded bg-sky-300 font-bold px-6 py-4 m-2 hover:cursor-pointer hover:brightness-105`} onClick={checkAnswer}>확인</div>
+                                        </div>
+                                    </div>
+
+
+
                                 </div>
-                        )
-                    })
-                    }
-                </div>
-                <div className='flex justify-end'>
-                    <div className={`font-bold px-6 py-4 mx-2 my-2 border bg-white hover:cursor-pointer hover:brightness-95 ${ game.started ? "hidden" : ""}`} onClick={intializeIcons}>초기화</div>
-                    <div className={`rounded ${ answer.no === "" ? "bg-gray-300" : "bg-sky-300"} font-bold px-6 py-4 my-2 hover:cursor-pointer hover:brightness-105 ${ game.started ? "hidden" : ""}`} onClick={startGame}>완료</div>
+                                <div style={{width: "480px"}} >
+                                    <div className="grid grid-rows-4 gap-3">
+                                        {icons.map((row, index) => {
+                                            return (
+                                                    <div key={index} style={{background: colors[index]}} className="rounded p-2 grid grid-cols-5 gap-3">
+                                                        <div className="flex items-center"><div className="text-center grow text-2xl text-bold text-white">{index+1}</div></div>
+
+                                                        {row.map((icon, index_i) => {
+                                                            return (
+                                                                <Icon key={index*4+index_i} row={index} no={index_i} icon={icons[index][index_i]["icon"]} desc={icons[index][index_i]["desc"]} onOpenModal={onOpenModal}/>
+                                                            )
+                                                        })}
+                                                        
+                                                    </div>
+                                            )
+                                        })
+                                        }
+                                    </div>
+                                    <div className='flex justify-end'>
+                                        <div className={`font-bold px-6 py-4 mx-2 my-2 border bg-white hover:cursor-pointer hover:brightness-95 ${ game.started ? "hidden" : ""}`} onClick={intializeIcons}>초기화</div>
+                                        <div className={`rounded ${ answer.no === "" ? "bg-gray-300" : "bg-sky-300"} font-bold px-6 py-4 my-2 hover:cursor-pointer hover:brightness-105 ${ game.started ? "hidden" : ""}`} onClick={startGame}>완료</div>
+                                    </div>
+                                </div>
+                                <>{modal.open && <IconList activeIcon={modal.activeIcon} row={modal.row} no={modal.no} selectIcon={selectIcon} onOpenModal={onOpenModal}/>}</>
+                            </div>
+
                 </div>
             </div>
-            <>{modal.open && <IconList activeIcon={modal.activeIcon} row={modal.row} no={modal.no} selectIcon={selectIcon} onOpenModal={onOpenModal}/>}</>
+            <Footer/>
         </div>
+
+
+        
     );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -3,36 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  HashRouter
-} from "react-router-dom";
-
-
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers'
-
 import Maker from './components/Maker';
-import Example from './components/Example';
-import Layout from './components/Layout';
 
-const store = createStore(reducers)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-              <Route path="maker" element={<Maker />}/>
-              <Route path="example" element={<Example />}/>
-          </Route>
-        </Routes>
-    </HashRouter>
-  </Provider>
+    <Maker/>
 );
 
 // If you want to start measuring performance in your app, pass a function
